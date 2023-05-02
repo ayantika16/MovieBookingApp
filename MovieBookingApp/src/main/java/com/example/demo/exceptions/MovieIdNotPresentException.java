1720b78120b367c183dee777c0e73b2aa03d43ad
Datta <2142474@cognizant.com>
2023-05-02 11:13:27 +0530

    Second Commit

--- MovieBookingApp/src/main/java/com/example/demo/exceptions/MovieIdNotPresentException.java
+++ MovieBookingApp/src/main/java/com/example/demo/exceptions/MovieIdNotPresentException.java
@@ -3,7 +3,7 @@ package com.example.demo.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code=HttpStatus.CONFLICT, reason="Movie Id Not Present, handled by Custom Exception")
+@ResponseStatus(code=HttpStatus.BAD_REQUEST, reason="Movie Id Not Present, handled by Custom Exception")
 public class MovieIdNotPresentException extends Exception{
 
 }
--- MovieBookingApp/src/main/java/com/example/demo/exceptions/NoMoviePresentException.java
+++ MovieBookingApp/src/main/java/com/example/demo/exceptions/NoMoviePresentException.java
@@ -3,7 +3,8 @@ package com.example.demo.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code=HttpStatus.NO_CONTENT, reason="No Movies Present in List, handled by Custom Exception")
+//@ResponseStatus(code=HttpStatus.NO_CONTENT, reason="No Movies Present in List, handled by Custom Exception")
+@ResponseStatus(code=HttpStatus.NO_CONTENT)
 public class NoMoviePresentException extends Exception{
 
 }
